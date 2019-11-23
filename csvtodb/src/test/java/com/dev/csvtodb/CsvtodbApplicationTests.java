c90968cf109da43bc3473f6af0c3d008c098e5be
ladnikhil <53752444+ladnikhil@users.noreply.github.com>
2019-11-23 16:38:20 +0530

    Add files via upload

---
+++ csvtodb/src/test/java/com/dev/csvtodb/CsvtodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.csvtodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsvtodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
