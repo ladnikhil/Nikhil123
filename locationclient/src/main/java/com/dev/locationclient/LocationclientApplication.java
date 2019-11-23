8c8deaaa528c85333dcc6c0ac295cf3c57ef15a2
ladnikhil <53752444+ladnikhil@users.noreply.github.com>
2019-11-23 17:01:26 +0530

    Add files via upload

---
+++ locationclient/src/main/java/com/dev/locationclient/LocationclientApplication.java
@@ -0,0 +1,13 @@
+package com.dev.locationclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocationclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocationclientApplication.class, args);
+	}
+
+}
---
+++ locationclient/src/test/java/com/dev/locationclient/LocationclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.locationclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocationclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
